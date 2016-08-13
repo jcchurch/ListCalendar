0f872b387baea155ba56505c5537eebcc4feb82b
James Church <jcchurch@gmail.com>
2016-08-13 13:30:03 -0500

    Includes new feature of passing the day of week for filtered printing. Also fixes one regular expression.

--- lcal.py
+++ lcal.py
@@ -5,7 +5,7 @@ import optparse
 import re
 
 def createDate(stringDate):
-    if re.match("\d\d\d\d-\d\d-\d\d", stringDate) is None:
+    if re.match("\d\d\d\d-\d\d-\d\d$", stringDate) is None:
         raise ValueError("This is not in the correct date format. Use YYYY-MM-DD")
 
     (yearStr, monthStr, dayStr) = stringDate.split("-")
@@ -27,21 +27,33 @@ def createDate(stringDate):
 
     return datetime.date(year, month, day)
 
-def displaySequentialDates(startStr, endStr):
+def displaySequentialDates(startStr, endStr, daysOfWeek):
     startDate = createDate(options.startDate)
     endDate = createDate(options.endDate)
 
     while startDate <= endDate:
-        print(startDate.strftime("%A. %B %d, %Y"))
+        if getLetterCodeForDayOfWeek(startDate) in daysOfWeek:
+            print(startDate.strftime("%A. %B %d, %Y"))
         startDate += datetime.timedelta(days=1)
+
+def getLetterCodeForDayOfWeek(date):
+    return "MTWRFSU"[ date.weekday() ]
 
 if __name__ == '__main__':
     desc = """List dates in sequential order"""
     p = optparse.OptionParser(usage="%prog [options] [file]", version="%prog 0.1", description=desc)
 
-    p.add_option("-s", "--start", dest="startDate", help="Start Date in YYYY-MM-DD", metavar="2016-01-01")
-    p.add_option("-e", "--end", dest="endDate", help="End Date in YYYY-MM-DD", metavar="2016-01-31")
+    p.add_option("-s", "--start", dest="startDate", help="Start Date in YYYY-MM-DD (Required)", metavar="2016-01-01")
+    p.add_option("-e", "--end", dest="endDate", help="End Date in YYYY-MM-DD (Required)", metavar="2016-01-31")
+    p.add_option("-d", "--dow", dest="daysOfWeek", help="Only print these days of the week: Monday, Tuesday, Wednesday, thuRsday, Friday, Saturday, sUnday. Default is MTWRFSU", metavar="MTWRFSU", default="MTWRFSU")
     options, arguments = p.parse_args()
 
+    if re.match("[MTWRFSUmtwrfsu]+$", options.daysOfWeek) is None:
+        raise ValueError("This is not in day of week pattern. Use only the appropriate 1 letter codes.")
+
+    options.daysOfWeek = options.daysOfWeek.upper()
+
     if options.startDate is not None and options.endDate is not None:
-        displaySequentialDates(options.startDate, options.endDate)
+        displaySequentialDates(options.startDate, options.endDate, options.daysOfWeek)
+    else:
+        print("Both a start date and an end date are required.")
